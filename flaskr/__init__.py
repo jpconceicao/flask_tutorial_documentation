1a951b8d44b9c6c6cbe333d889f09570e467f120
Jorge Palma Conceição <jorgepalmaconceicao@gmail.com>
2022-10-10 15:06:57 -0300

    start blog blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -32,4 +32,7 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app) # Inclui o comando de inicialização do banco
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
